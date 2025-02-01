43ff967a967d7e3c6cda96a7c6bc001ae440482e
anantmahambrey <anantmahambrey@gmail.com>
2025-02-02 02:22:37 +0530

    Changed UI

--- editor/urls.py
+++ editor/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('run-code/', views.run_code, name='run_code'),
+    path('process-sidebar/', views.process_sidebar, name='process_sidebar'),
 ]
--- editor/views.py
+++ editor/views.py
@@ -3,10 +3,12 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_protect
 import json
 import requests
+import google.generativeai as genai
 
 def index(request):
     return render(request, 'editor/index.html')
 
+#On run code button
 @csrf_protect
 def run_code(request):
     if request.method == 'POST':
@@ -18,15 +20,15 @@ def run_code(request):
             inputs = data.get('inputs', '')
 
             mapping = {'text/x-java':'4','python':'5','text/x-csrc':'6','text/x-c++src':'7','javascript':'17'}
-            lang=mapping[language]
+            lang=mapping[language]   #language for compiler
 
             # Compiler API configuration
             url = "https://code-compiler.p.rapidapi.com/v2"
             
             payload = {
-                "LanguageChoice": lang,  # Python
-                "Program": code,  # Use the actual code from the editor
-                "Input": inputs
+                "LanguageChoice": lang,  # language
+                "Program": code,  # The actual code from the editor
+                "Input": inputs  # inputs from editor if any
             }
             
             headers = {
@@ -65,3 +67,47 @@ def run_code(request):
     return JsonResponse({
         'error': 'Method not allowed'
     }, status=405)
+
+
+#On ask AI (submit) button
+@csrf_protect
+def process_sidebar(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            text = data.get('text', '')
+            purpose = data.get('purpose', '')
+            
+            #sending to Gemini
+            genai.configure(api_key="AIzaSyDxiigaZNe4TTzdDojZOoQfOC-PjnTLiw4")
+            generation_config = {
+                "temperature": 1,
+                "top_p": 0.95,
+                "top_k": 40,
+                "max_output_tokens": 8192,
+                "response_mime_type": "text/plain",
+            }
+            model = genai.GenerativeModel(
+                model_name="gemini-1.5-flash",
+                generation_config=generation_config,
+            )
+
+            chat = model.start_chat()
+            if purpose=='a':
+                prompt='Generate a code snippet for the following. Do not include any other text, or letters or punctuations. Just give one code. Thats it.'
+            elif purpose=='b':
+                prompt='Debug the following code. Briefly explain what is wrong and what can be corrected.'
+            else:
+                prompt='Please answer the following query briefly.'
+            
+            warning ='Do Not include any Bold Words.'
+            prompt+= text
+            prompt+=warning
+            ai_response = chat.send_message(prompt)
+
+            return JsonResponse({'output': ai_response.text})  # Sending result back.
+        except json.JSONDecodeError as e:
+            return JsonResponse({'error': 'Invalid JSON format'}, status=400)
+        except Exception as e:
+            return JsonResponse({'error': str(e)}, status=500)
+    return JsonResponse({'error': 'Method not allowed'}, status=405)
